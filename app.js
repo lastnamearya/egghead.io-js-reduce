d9157cc177caf394b687aa3bb7783e4d7c7ae3b4
lastnamearya <arya.jigyasu6815@gmail.com>
2019-07-15 01:10:18 +0530

    Flattig nested objects using ~ map and reduce

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-// Flatten an array of arrays using reduce.
+// Flatting nested objects using map and reduce.
 
-const flattened = [[0, 1], [2, 3], [4, 5]];
+const nestedArray = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
 
-const result = flattened.reduce(function(accumulator, currentValue) {
-  return accumulator.concat(currentValue);
-}, []);
+// My current approach through map
+
+const result = nestedArray.map(({ id }) => id);
 
 console.log(result);
+
+// New approach using ~ reduce
+
+const reduceResult = nestedArray.reduce(function(accumulator, currentValue) {
+  return accumulator.concat(currentValue.id);
+}, []);
+
+console.log(reduceResult);
