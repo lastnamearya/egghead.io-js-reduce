67565a47a9b1ec75613c4372d3b741c40e521d2e
lastnamearya <arya.jigyasu6815@gmail.com>
2019-07-15 01:04:58 +0530

    Array flattened through reduce

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-// Reduce ~ accumulating properties of objects inside an array.
+// Flatten an array of arrays using reduce.
 
-var sum = [{ x: 1 }, { x: 2 }, { x: 3 }];
+const flattened = [[0, 1], [2, 3], [4, 5]];
 
-// If we forget to pass the initial value as an additional argument then accumulator will be the first value of sum array.
-
-var result = sum.reduce(function(accumulator, currentValue) {
-  return accumulator + currentValue.x;
-});
-
-// It'll log ~ [object object]23 ~ The reason is we forget to pass the initialValue to accumulator and it will be become [object object] + 2 + 3 ~ That coerces to [object object]23
+const result = flattened.reduce(function(accumulator, currentValue) {
+  return accumulator.concat(currentValue);
+}, []);
 
 console.log(result);
-
-// Passed initial value explicity.
-
-var initialValue = 0;
-
-var newResult = sum.reduce(function(accumulator, currentValue) {
-  return accumulator + currentValue.x;
-}, initialValue);
-
-// It'll be 6.
-
-console.log(newResult);
