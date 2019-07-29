0c6f1f9950edba57092518f8015ae9e0649adbf8
lastnamearya <arya.jigyasu6815@gmail.com>
2019-07-29 08:28:25 +0530

    in operator ~ to check if an property exists in an object or not

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-// Counting instances of values in an object like how many times particular property occurs.
+// How to check if a property exists in an Object
 
-const namesArray = ["Alice", "Bob", "Tiff", "Bruce", "Alice", "Bob", "Alice"];
+const object = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4
+};
 
-const resultArray = namesArray.reduce(function(accumulator, currentValue) {
-  // Here it's written first but executed later after else, If there's currentValue property already exists in accumulator which has value of 1. If we found it then we increased it by 1.
+// using in operator we can check if a property exists in an object or not.
 
-  if (currentValue in accumulator) {
-    accumulator[currentValue]++;
-  } else {
-    // Here we first add the curretValue property to accumualator and set it value to 1.
-
-    accumulator[currentValue] = 1;
-  }
-
-  // In last we return our accumulator Object. Why object ~ because we explicity passed empty object as an initial value for the accumulator.
-
-  return accumulator;
-}, {});
-
-console.log(resultArray);
+console.log("a" in object);
