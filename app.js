7ccc0a9584849e18f4c54d0e1f90e2620830d991
lastnamearya <arya.jigyasu6815@gmail.com>
2019-08-03 16:20:22 +0530

    Problem ~ Find sum of non-repeated integer in an array ~ Solution corrected

--- app.js
+++ app.js
@@ -1,56 +1,61 @@
-// Problem ~ Using reduce return an array with all the cast members and only include one star once.
+/* Find the sum of Numbers in an array that are not repeated ~ Like if one number for example 2 repeated more then once then dont' add it in whole sum.
 
-const data = [
-  {
-    title: "Batman Begins",
-    year: 2005,
-    cast: [
-      "Christian Bale",
-      "Michael Caine",
-      "Liam Neeson",
-      "Katie Holmes",
-      "Gary Oldman",
-      "Cillian Murphy"
-    ]
-  },
-  {
-    title: "The Dark Knight",
-    year: 2008,
-    cast: [
-      "Christian Bale",
-      "Heath Ledger",
-      "Aaron Eckhart",
-      "Michael Caine",
-      "Maggie Gyllenhal",
-      "Gary Oldman",
-      "Morgan Freeman"
-    ]
-  },
-  {
-    title: "The Dark Knight Rises",
-    year: 2012,
-    cast: [
-      "Christian Bale",
-      "Gary Oldman",
-      "Tom Hardy",
-      "Joseph Gordon-Levitt",
-      "Anne Hathaway",
-      "Marion Cotillard",
-      "Morgan Freeman",
-      "Michael Caine"
-    ]
+*/
+
+const sampleArray = [1, 2, 2, 3, 2, 4];
+
+// Right Solution ~ My non-optimized Solution
+
+const intermediatoryArray = [];
+const duplicateArray = [];
+
+const newResult = sampleArray.reduce(function(
+  accumulator,
+  currentValue,
+  index,
+  array
+) {
+  if (intermediatoryArray.indexOf(currentValue) === -1) {
+    intermediatoryArray.push(currentValue);
+  } else {
+    duplicateArray.push(currentValue);
   }
-];
 
-const result = data.reduce(function(accumulator, currentValue) {
-  currentValue.cast.forEach(star => {
-    // if idexOf === -1, It means star is not included in the array itself.
-    if (accumulator.indexOf(star) === -1) {
-      accumulator.push(star);
-    }
-    // No else here ~ because we're only going to add those stars which are not inluded in the accumulator.
-  });
+  if (index === array.length - 1) {
+    intermediatoryArray.forEach(value => {
+      if (duplicateArray.indexOf(value) === -1) {
+        accumulator += value;
+      }
+    });
+  }
+
   return accumulator;
-}, []);
+},
+0);
 
-console.log(result);
+console.log(newResult);
+
+// **************************************************** //
+
+/* Old Solution ~ This is not correct solution because it'll break when we apply it for values [1, 2, 3, 2, 2, 4] ~ When a element repeated odd no of times like 3. It'l break.
+
+*/
+
+// It's sum needs to be 1 + 3 + 4 = 8
+
+// const intermediatoryArray = [];
+
+// const result = array.reduce(function(accumulator, currentValue) {
+//   if (!intermediatoryArray.includes(currentValue)) {
+//     intermediatoryArray.push(currentValue);
+//     accumulator += currentValue;
+//   } else {
+//     accumulator -= currentValue;
+//   }
+
+//   return accumulator;
+// }, 0);
+
+// // It'll be 6 rather than 8.
+
+// console.log(result);
