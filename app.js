aa1faf796b580fa0a481de295be6df4d1460091c
lastnamearya <arya.jigyasu6815@gmail.com>
2019-08-03 23:27:32 +0530

    reduceRight ~ right to left accumulation

--- app.js
+++ app.js
@@ -1,82 +1,11 @@
-/* Find the sum of Numbers in an array that are not repeated ~ Like if one number for example 2 repeated more then once then dont' add it in whole sum.
+// reduceRight ~ Now our accumulation will starts from right to left. Default accumulation will be from left to right. It's handy and good to have in our Toolbox.
 
-*/
+const data = [1, 2, 3, 4, "5"];
 
-const sampleArray = [1, 2, 2, 3, 2, 4];
+// result will be ~ 054321 ( String )
 
-// Set ~ removed only duplicatedValues Array we'll use.
+const result = data.reduceRight(function(accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0);
 
-function arraySum(array) {
-  // Duplicate Array ~ Hold duplicate integers that occurs more than one time ~ [2, 2]
-
-  return array.reduce(function(accumulator, currentValue) {
-    const duplicatedValues = array.filter(
-      (num, index) => array.indexOf(num) !== index
-    );
-
-    if (duplicatedValues.indexOf(currentValue) === -1) {
-      accumulator += currentValue;
-    }
-    return accumulator;
-  }, 0);
-}
-
-console.log(arraySum(sampleArray));
-
-// **************************************************** //
-
-// Right Solution ~ My non-optimized Solution
-
-const intermediatoryArray = [];
-const duplicateArray = [];
-
-const newResult = sampleArray.reduce(function(
-  accumulator,
-  currentValue,
-  index,
-  array
-) {
-  if (intermediatoryArray.indexOf(currentValue) === -1) {
-    intermediatoryArray.push(currentValue);
-  } else {
-    duplicateArray.push(currentValue);
-  }
-
-  if (index === array.length - 1) {
-    intermediatoryArray.forEach(value => {
-      if (duplicateArray.indexOf(value) === -1) {
-        accumulator += value;
-      }
-    });
-  }
-
-  return accumulator;
-},
-0);
-
-// console.log(newResult);
-
-// **************************************************** //
-
-/* Old Solution ~ This is not correct solution because it'll break when we apply it for values [1, 2, 3, 2, 2, 4] ~ When a element repeated odd no of times like 3. It'l break.
-
-*/
-
-// It's sum needs to be 1 + 3 + 4 = 8
-
-// const intermediatoryArray = [];
-
-// const result = array.reduce(function(accumulator, currentValue) {
-//   if (!intermediatoryArray.includes(currentValue)) {
-//     intermediatoryArray.push(currentValue);
-//     accumulator += currentValue;
-//   } else {
-//     accumulator -= currentValue;
-//   }
-
-//   return accumulator;
-// }, 0);
-
-// // It'll be 6 rather than 8.
-
-// console.log(result);
+console.log(result);
