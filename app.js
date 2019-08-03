59be93f46bdc9bcab3d1f44f71a24e687fd3bc5d
lastnamearya <arya.jigyasu6815@gmail.com>
2019-08-04 00:39:16 +0530

    inspect nested objects with Reduce

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
-// Reactive Programming.
+var luke = {
+  name: "luke skywalker",
+  jedi: true,
+  parents: {
+    father: {
+      jedi: true
+    },
+    mother: {
+      jedi: false
+    }
+  }
+};
 
-// Pipeline ~ Composing functions with reduce
+var han = {
+  name: "han solo",
+  jedi: false,
+  parents: {
+    father: {
+      jedi: false
+    },
+    mother: {
+      jedi: false
+    }
+  }
+};
 
-function increment(input) {
-  return input + 1;
+var anakin = {
+  name: "anakin skywalker",
+  jedi: true,
+  parents: {
+    mother: {
+      jedi: false
+    }
+  }
+};
+
+var characters = [luke, han, anakin];
+
+function fatherWasJedi(character) {
+  var path = "parents.father.jedi";
+  return path.split(".").reduce(function(obj, field) {
+    if (obj) {
+      return obj[field];
+    }
+
+    return false;
+  }, character);
 }
-function decrement(input) {
-  return input - 1;
-}
-function double(input) {
-  return input * 2;
-}
-function halve(input) {
-  return input / 2;
-}
 
-// A series of functions that get applied some initial value in order to return some final value and this pipeline can be invoked as just single function.
-
-const pipeline = [increment, double, decrement];
-
-const initialValue = 1;
-
-const result = pipeline.reduce(function(accumulator, currentFunc) {
-  return currentFunc(accumulator);
-}, initialValue);
-
-console.log(result);
+characters.forEach(function(character) {
+  console.log(
+    character.name + "'s father was a jedi:",
+    fatherWasJedi(character)
+  );
+});
